53d52e07fa943c5e801b9f4075d01bfe81d1bbf2
addwis <addwis.jp@gmail.com>
2020-04-22 23:12:49 +0900

    Add: 下書き取得用にテーマを追加

---
+++ html/wp-content/themes/wp-headless-theme/functions.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Theme Functions
+ *
+ * @package wp-headless
+ */
+
+// CORS対策
+function cors_http_header(){
+    header("Access-Control-Allow-Origin: *");
+}
+add_action('send_headers', 'cors_http_header');
+
+// nonce生成
+function custom_preview_page_link($link) {
+  global $post;
+  $id = $post->ID;
+  // $prefix = $post->post_type;
+  // $id = get_the_ID();
+  $nonce = wp_create_nonce('wp_rest');
+  $link = 'http://localhost:8000/preview/?id='. $id. '&_wpnonce='. $nonce;
+  return $link;
+}
+add_filter('preview_post_link', 'custom_preview_page_link');
+
+// add_filter( 'rest_prepare_revision', function( $response, $post ) {
+//   $data = $response->get_data();
+//   $data['acf'] = get_fields( $post->ID );
+
+//   return rest_ensure_response( $data );
+// }, 10, 2 );
+
+// function custom_preview_page_link( $link ) {
+// 	$id = get_the_ID();
+// 	// For authentication a nonce must be created
+// 	// otherwise the REST API does not allow users to see previews
+// 	$nonce = wp_create_nonce( 'wp_rest' );
+// 	$post = get_post( $id );
+// 	$slug = $post->post_name;
+// 	$lang = '';
+// 	$isPost = '';
+
+// 	// if we preview a post, we want to add 'posts/' to the url to link to our posts site in the frontend
+// 	// TODO this URL schould be dynamicly the same for back- and frontend
+// 	if ($post->post_type === 'post') {
+// 		$isPost = '';
+// 	}
+
+// 	// if we have a new page without a slug we show /preview/ as a page
+// 	if ($slug === '') {
+// 		$slug = 'preview';
+// 	}
+
+// 	// Get the language of the polylang custom field
+// 	// https://polylang.wordpress.com/documentation/documentation-for-developers/functions-reference/
+// 	$current_lang = pll_get_post_language( $id );
+// 	$default_lang = pll_default_language( 'slug' );
+
+// 	// Only add a language if it's not the default language
+// 	if ($current_lang !== $default_lang) {
+// 		$lang = $current_lang . '/';
+// 	}
+
+// 	/* make sure WP_HOME (wp-config.php) always has exactly one trailing slash.
+// 	Remove if it has multiple then add one or add one if it has no slash */
+// 	$home_path = rtrim(WP_HOME, '/') . '/';
+// 	$link = $home_path . $lang . $isPost . $slug . '/?preview_id='. $id. '&preview_nonce=' . $nonce . '&preview=true';
+// 	return $link;
+// }
+// add_filter('preview_post_link', 'custom_preview_page_link');
---
+++ html/wp-content/themes/wp-headless-theme/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Redirect individual post and pages to the REST API endpoint
+ *
+ * @package wp-headless
+ */
+
+wp_die( "wp-headless-theme" );
